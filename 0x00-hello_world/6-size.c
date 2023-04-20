147f9d41ef56f1cc8f2b3518f2fc863b3000e1a4
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-04-20 11:38:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
-   * This program demonstrates the use of the sizeof operator
+   *main - Entry point
    * to determine the size of various data types in C
    * Return: Always 0 (Success)
    */
@@ -8,6 +8,7 @@ int main(void)
 {
 	printf("Size of a char : %d byte(s) \n", sizeof(char));
 	printf("Size of an int: %d byte(s) \n", sizeof(int));
+	printf("Size of a long long int : %d byte(s) \n", sizeof(long int));
 	printf("Size of a long long int : %d byte(s) \n", sizeof(long long int));
 	printf("Size of a float : %d byte(s) \n", sizeof(float));
 	return (0);
