f0c052c10c284c7a5ef8f199dc5cf3535f918f22
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-04-20 06:48:21 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main -continue puts function that print a message
+  *puts - print a message
+  * return return 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
