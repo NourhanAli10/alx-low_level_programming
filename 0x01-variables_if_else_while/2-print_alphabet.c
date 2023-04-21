237bbb030f913875d442b67ada3eceaeb9127b94
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-04-21 09:52:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alpha = 'a';
+for (alpha; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+return (0);
+}
