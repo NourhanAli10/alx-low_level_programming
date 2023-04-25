4c7afc77edf0a2b275d199ccd87480d012b324e7
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-04-25 14:33:00 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * _islower - checks for lowercase character
+  * Returns 1 if c is lowercase
+  * Returns 0 otherwise
+  * @c: the character in ASCi table
+  * Return: 1 for lowercase. 0 for others
+  */
+int _islower(int c)
+{
+	int n;
+
+	if (n >= 95 && n <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+
+}
+
