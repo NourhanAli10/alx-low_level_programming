7725ca34940c521a8037f3dfb7589a6e4f6b3ba4
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-04-25 11:54:48 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,33 @@
 * _putchar - writes the character c to stdout
 * Return: Always 0 (Success).
 */
+void print_word(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+}
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+print_word();
 return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
