5fc49f28b3d4210cbe516df782ed7a8118f87c7a
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-04-27 14:19:54 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,25 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
+			
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
+		
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
+			
 		}
 		else
 		{
 			printf("%d", i);
-			printf("  ");
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
