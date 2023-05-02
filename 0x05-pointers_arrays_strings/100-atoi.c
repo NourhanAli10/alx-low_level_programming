91f132e3a73864f01623b6fff7abf3528a68721b
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-05-02 02:47:16 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+include "main.h"
+/**
+* _atoi - function that convert a string to an integer.g
+* @s: the pointer that store the value of the string
+* Return: sign * num
+*/
+int _atoi(char *s)
+{
+int sign = 1;
+int num = 0;
+int i = 0;
+while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+i++;
+if (s[i] == '-')
+{
+sign = -1;
+i++;
+}
+else if (s[i] == '+')
+{
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+num = (num * 10) + (s[i] - '0');
+i++;
+}
+return (sign *num);
+}
