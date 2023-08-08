aa47f484873048fd03d060d24f73600b032d58dc
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-08-08 05:30:43 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,18 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	array = malloc((sizeof(char)) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
-	}
-	if (size == 0 || array == NULL)
-	{
-		return (NULL);
 	}
 	return (array);
 }
