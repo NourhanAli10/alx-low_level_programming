63ec46b57b611eb4384f1cb9cf260a3c5611f0a4
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-08-17 12:25:44 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,4 +19,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	        printf("%s%s", (ptr == NULL) ? "nil" : ptr, (i < n - 1 && separator != NULL) ? separator : "");
 	}
 	printf("\n");
+	va_end(args);
 }
