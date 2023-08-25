ee8ad5c019ca8f592a0cc3ede058d82bc7b49e98
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-08-25 06:26:43 -0700

    edit first file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void print_first(void)__attribute__ ((constructor));
 
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("%s", "I bore my house upon my back!\n");
 }
