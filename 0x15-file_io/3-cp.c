f9d83a949bab2a32a6e68ab3b19417e2c172d936
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2023-09-04 16:45:12 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 
     fn_from = open(file_from, O_RDONLY);
 
-    if (file_from == NULL || fn_from == -1)
+    if (fn_from == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
         exit(98);
