75d7ce28fcf88c1efa7b463ff2075ef429a5e578
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-02-07 20:58:32 +0200

    tasks

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -8,45 +8,32 @@
 * @array: Pointer to the first element of the array
 * @size: Number of elements in the array
 * @value: Value to search for
-*
 * Return: Index where the value is located, or -1 if not found or array is NULL
 */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low, high;
-	size_t numerator, denominator;
-	size_t pos;
+	size_t pos = 0, low = 0, high = size - 1;
 
-	if (array == NULL || size == 0)
+	if (!array || !size)
 		return (-1);
 
-	low = 0;
-	high = size - 1;
-
-	while (low <= high && value >= array[low] && value <= array[high])
+	while (1)
 	{
-		numerator = value - array[low];
-		denominator = array[high] - array[low];
-
-		if (denominator == 0)
+		pos = low + (((double)(high - low) /
+			  (array[high] - array[low])) * (value - array[low]));
+		if (pos >= size)
 		{
-			printf("Denominator is zero! Cannot proceed.\n");
-			return (-1);
+			printf("Value checked array[%lu] is out of range\n", pos);
+			break;
 		}
-
-		pos = low + ((double)(high - low) / denominator) * numerator;
-
-		printf("Value checked array[%lu] = [%d], pos = %lu\n", pos, array[pos], pos);
-
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
-
-		if (array[pos] < value)
-			low = pos + 1;
-		else
+		else if (array[pos] > value)
 			high = pos - 1;
+		else
+			low = pos + 1;
 	}
-
-	printf("Value checked array[%lu] is out of range\n", low);
 	return (-1);
 }
